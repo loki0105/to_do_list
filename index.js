c663ad1a975b9e178a8af6561dff48078eab62fc
Abdumalik Umarov <63663677+loki0105@users.noreply.github.com>
2025-01-20 23:15:44 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+ 
+
+const db= new pg.Client({
+	user: "postgres",
+	password: "4909770",
+	host: "localhost",
+  database: "users",
+  port: 5432
+});
+db.connect();
+
+
+app.get("/", async (req, res) => {
+	const result = await db.query("SELECT * from items order by id asc");
+	console.log(result.rows);
+	let items = result.rows;
+	res.render("index.ejs", {
+		listTitle: "Today",
+		listItems: items,
+	});
+	console.log(items);
+});
+
+app.post("/add", (req, res) => {
+  const item = req.body.newItem;
+  db.query("insert into items (title) values ($1)", [item]);
+  
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {
+  const edit_id= req.body.updatedItemId;
+const edit_title = req.body.updatedItemTitle;
+   db.query("UPDATE items SET title = $1 WHERE id = $2;", [
+			edit_title,
+			edit_id
+		]);
+   res.redirect("/");
+
+});
+
+app.post("/delete", (req, res) => {
+   const dl = req.body.deleteItemId;
+  
+  
+db.query("delete from items where id = $1", [dl]);
+  
+		res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
